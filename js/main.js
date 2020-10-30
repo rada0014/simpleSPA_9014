9df3f6367671cd2ccf95208e5496527ab0b30c27
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2020-10-30 11:30:03 -0400

    initial commit of starter files

---
+++ js/main.js
@@ -0,0 +1,18 @@
+//app is for general control over the application
+//and connections between the other components
+const APP = {};
+
+//search is for anything to do with the fetch api
+const SEARCH = {};
+
+//actors is for changes connected to content in the actors section
+const ACTORS = {};
+
+//media is for changes connected to content in the media section
+const MEDIA = {};
+
+//storage is for working with localstorage
+const STORAGE = {};
+
+//nav is for anything connected to the history api and location
+const NAV = {};
